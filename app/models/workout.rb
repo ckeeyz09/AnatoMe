9cf304151ac13e0d78871d4c569b4aa205e42216
Ian Civgin <imciv2@gmail.com>
2015-08-17 16:56:53 -0700

    updated splash page and login page

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,6 +2,6 @@ class Workout < ActiveRecord::Base
   has_many :appointments
   has_many :users, through: :appointments
 
-  extend FriendlyId
-  friendly_id :name, use: :slugged
+  # extend FriendlyId
+  # friendly_id :name, use: :slugged
 end
